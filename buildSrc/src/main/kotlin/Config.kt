8439f1aa045b75f64ea4c7682d37c9bc4890dd2d
Sidali Mellouk <s.mellouk@outlook.com>
2020-05-01 14:53:28 +0200

    Task-1 [Prj][New]: Create project configuration * Added buildSrc module to handle dependencies and versions * Added repository module which will represent data repo for our domain * Added core module which will represent our core business * Added widget module which will be the client presentation

---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,10 @@
+object Config {
+    const val compileSdkVersion = 29
+    const val buildToolsVersion = "29.0.3"
+    const val appId = "io.mellouk.musiget"
+    const val mindSdkVersion = 19
+    const val targetSdkVersion = 29
+    const val javaVersion = "1.8"
+    const val applicationPlugin = "com.android.application"
+    const val libraryPlugin = "com.android.library"
+}
